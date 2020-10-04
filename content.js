2d360f74247b20116336e7f6f3013db7c9905068
Zizhen Song <zizhensong@gmail.com>
2020-10-04 13:32:19 -0700

    locally stores clickcount

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
 console.log('hello, I am a content script');
+window.addEventListener('click', click);
+function click(e) {
+  if (localStorage.clickcount) {
+    localStorage.clickcount = Number(localStorage.clickcount) + 1;
+  } else {
+    localStorage.clickcount = 1;
+  }
+  console.log(localStorage.clickcount);
+}
